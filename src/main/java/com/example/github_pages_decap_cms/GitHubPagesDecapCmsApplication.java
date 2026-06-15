0c79da55bf643d0fe28becde52b5e05a9e88bf57
VadimKharovyuk <146035315+VadimKharovyuk@users.noreply.github.com>
2026-06-15 16:58:58 +0400

    Initial commit

---
+++ src/main/java/com/example/github_pages_decap_cms/GitHubPagesDecapCmsApplication.java
@@ -0,0 +1,13 @@
+package com.example.github_pages_decap_cms;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GitHubPagesDecapCmsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GitHubPagesDecapCmsApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/github_pages_decap_cms/GitHubPagesDecapCmsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.github_pages_decap_cms;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GitHubPagesDecapCmsApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
