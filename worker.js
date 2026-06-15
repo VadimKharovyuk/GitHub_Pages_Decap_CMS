c621d00b6552410e8f6a5dd5a76a9850a62f41d8
VadimKharovyuk <146035315+VadimKharovyuk@users.noreply.github.com>
2026-06-15 17:59:37 +0400

    update worker.js

--- worker.js
+++ worker.js
@@ -3,23 +3,47 @@ export default {
         const url = new URL(request.url);
 
         if (url.pathname === "/auth") {
-            const redirect = `https://github.com/login/oauth/authorize?client_id=${env.CLIENT_ID}&scope=repo`;
+            const redirect = `https://github.com/login/oauth/authorize?client_id=${env.CLIENT_ID}&scope=repo,user`;
             return Response.redirect(redirect, 302);
         }
 
         if (url.pathname === "/callback") {
             const code = url.searchParams.get("code");
+
             const response = await fetch("https://github.com/login/oauth/access_token", {
                 method: "POST",
                 headers: { "Content-Type": "application/json", "Accept": "application/json" },
-                body: JSON.stringify({ client_id: env.CLIENT_ID, client_secret: env.CLIENT_SECRET, code }),
+                body: JSON.stringify({
+                    client_id: env.CLIENT_ID,
+                    client_secret: env.CLIENT_SECRET,
+                    code,
+                }),
             });
+
             const data = await response.json();
             const token = data.access_token;
-            const script = `<script>window.opener.postMessage('authorization:github:success:{"token":"${token}"}','*');window.close();</script>`;
-            return new Response(script, { headers: { "Content-Type": "text/html" } });
+            const error = data.error;
+
+            const html = error
+                ? `<script>window.opener.postMessage('authorization:github:error:${error}','*');window.close();</script>`
+                : `<html><body><script>
+            (function() {
+              function receiveMessage(e) {
+                window.opener.postMessage(
+                  'authorization:github:success:${JSON.stringify({ token: "__TOKEN__" }).replace("__TOKEN__", token)}',
+                  e.origin
+                );
+              }
+              window.addEventListener("message", receiveMessage, false);
+              window.opener.postMessage("authorizing:github", "*");
+            })();
+          </script></body></html>`;
+
+            return new Response(html, {
+                headers: { "Content-Type": "text/html" },
+            });
         }
 
         return new Response("OK");
-    }
+    },
 };
